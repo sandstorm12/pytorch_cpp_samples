2d9e6ab13b98ea1a4b6d97167203cf47aa832f95
sandstorm12 <sandstormeatwo@gmail.com>
2024-12-26 15:23:40 -0700

    Minor comments in the sample base

--- sample_base/sample_base.cpp
+++ sample_base/sample_base.cpp
@@ -3,11 +3,14 @@
 #include <torch/torch.h>
 
 int main(){
+    // Check if CUDA is available
     torch::Device device(torch::kCPU);
     if (torch::cuda::is_available()) {
+        // Select the first GPU
         device = torch::Device(torch::kCUDA, 0);
     }
 
+    // Create a random tensor
     torch::Tensor x = torch::randn({3, 3}, device);
     std::cout << x << std::endl;
 
